05c210b7801cc0531bc382460f5b1863d6aa3a75
Licykk <licykk@gmail.com>
2018-11-10 17:35:29 -0500

    logo

--- oddjobs/oddjobsapp/views.py
+++ oddjobs/oddjobsapp/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from oddjobsapp.models import User, Post
+from socialapp.forms import SignUpForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 class Index(generic.ListView):
 
