67f545027727c7bce14bcd9715b6aabec0f813cc
AlkaSeltzers <32341871+AlkaSeltzers@users.noreply.github.com>
2018-11-10 18:03:01 -0500

    Account Settings coppied
    Error in Account Settings so coppied to paste again

--- oddjobs/oddjobsapp/templates/home/index.js
+++ oddjobs/oddjobsapp/templates/home/index.js
@@ -1,17 +1,56 @@
-# install directly with gem
-gem install "stream-ruby"
+// install via npm
+npm install getstream --save
 
-# or add this to your Gemfile and then run bundler
-gem "stream-ruby"
+// install using bower
+bower install getstream
 
-require 'stream'
-client = Stream::Client.new('7a7vvthtmw7d', 'dxm9bj3zdgvf84jq4m9h362svdmz8neqfryptc9jsfkhur5nz5tbvmhyt9arbrqd', :location => 'us-east')
+// latest build is available at this link as well
+https://raw.githubusercontent.com/GetStream/stream-js/master/dist/js/getstream.js
 
-alka = client.feed('user', 'alka')
+var stream = require('getstream');
+// Instantiate a new client (server side)
+client = stream.connect('7a7vvthtmw7d', 'dxm9bj3zdgvf84jq4m9h362svdmz8neqfryptc9jsfkhur5nz5tbvmhyt9arbrqd', 'us-east');
+// Instantiate a new client (client side)
+client = stream.connect('7a7vvthtmw7d', null, 'us-east');
+// Find your API keys here https://getstream.io/dashboard/
 
-activity_data = { :actor => 'alka', :verb => 'add', :object => 'picture:10', :foreign_id => 'picture:10', :message => 'Beautiful bird!' }
-alka.add_activity(activity_data);
+var chris = client.feed('user', 'chris');
 
-alka.remove_activity('picture:10', foreign_id=true)
+// Add an Activity; message is a custom field - tip: you can add unlimited custom fields!
+chris.addActivity({
+  actor: 'chris',
+  verb: 'add',
+  object: 'picture:10',
+  foreign_id: 'picture:10',
+  message: 'Beautiful bird!'
+}).then(
+  null, // nothing further to do
+  function(err) {
+    // Handle or raise the Error.
+  }
+);
 
-document.getElementById("stream").innerHTML=alka
+// Create a following relationship between Jack's "timeline" feed and Chris' "user" feed:
+var jack = client.feed('timeline', 'jack');
+jack.follow('user', 'chris').then(
+  null, // nothing further to do
+  function(err) {
+    // Handle or raise the Error.
+  }
+);
+
+
+// Read Jack's timeline and Chris' post appears in the feed:
+jack.get({ limit: 10 }).then(function(results) {
+  var activityData = results; // work with the feed activities
+},function(err) {
+    // Handle or raise the Error.
+});
+
+// Remove an Activity by referencing it's Foreign Id:
+chris.removeActivity({ foreignId: 'picture:10' }).then(
+  null, // nothing further to do
+  function(err) {
+    // Handle or raise the Error.
+  }
+);
